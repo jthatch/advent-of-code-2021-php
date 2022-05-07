501ae0a3412c39e81488b259608859e521ed0c99
James Thatcher <james@jameswt.com>
2022-05-07 12:15:08 +0100

    Day 5 update - tidy up buildMap() method

--- src/Day5.php
+++ src/Day5.php
@@ -13,12 +13,10 @@ class Day5 extends DayBehaviour
     {
         $map = $this->buildMap(input: $this->input, removeDiagonals: true);
 
-        // return points with more than 1 overlap
         return collect($map)
             ->sum(
-                fn ($pos): int           => collect($pos)
-                    ->filter(fn (int $p) => $p > 1)
-                    ->count()
+                // return points with more than 1 overlap
+                fn ($pos): int => collect($pos)->filter(fn (int $p) => $p > 1)->count()
             );
     }
 
@@ -26,12 +24,9 @@ class Day5 extends DayBehaviour
     {
         $map = $this->buildMap(input: $this->input);
 
-        // return points with more than 1 overlap
         return collect($map)
             ->sum(
-                fn ($pos): int           => collect($pos)
-                    ->filter(fn (int $p) => $p > 1)
-                    ->count()
+                fn ($pos): int => collect($pos)->filter(fn (int $p) => $p > 1)->count()
             );
     }
 
@@ -47,14 +42,18 @@ class Day5 extends DayBehaviour
         $map = [];
 
         collect($input)
+            // extract [x1,y1,x2,y2] integers from line
             ->map(fn (string $line) => sscanf($line, '%d,%d -> %d,%d'))
+            // for part 1, we remove any lines that aren't horizontal or vertical
+            // by checking that x1 = x2 or y1 = y2
             ->when(
                 $removeDiagonals,
                 fn (Collection $collection) => $collection->filter(
-                    // horizontals are when x1 = x2 or y1 = y2
-                    fn (array $pos) => $pos[0] === $pos[2] || $pos[1] === $pos[3]
+                    fn (array $pos): bool   => $pos[0] === $pos[2] || $pos[1] === $pos[3]
                 )
             )
+            // loop over each line and populate our map
+            // the point increments each time a line overlaps another
             ->eachSpread(function (int $x1, int $y1, int $x2, int $y2) use (&$map): void {
                 $xRange = range($x1, $x2);
                 $yRange = range($y1, $y2);
