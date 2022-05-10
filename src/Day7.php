ccf5d96fcc9654a52fc1bb4bb3edd38cf8101164
James Thatcher <james@jameswt.com>
2022-05-10 21:16:38 +0100

    Day 7 - improve delta reduce, drop array_sum and cache

--- src/Day7.php
+++ src/Day7.php
@@ -8,35 +8,45 @@ use App\Contracts\DayBehaviour;
 
 class Day7 extends DayBehaviour
 {
-    protected array $cache = [];
-
     public function solvePart1(): ?int
     {
         $input = array_map('intval', explode(',', $this->input[0]));
-        sort($input);
-        $crabs     = collect($input);
-        $positions = collect($input)->unique()->sort()->values();
+        // create histogram based on distance between crab and target position
+        $deltaReduce = fn (int $from, int $to): int => abs($from - $to);
 
-        return (int) $positions
-            // create histogram based on distance between crab and target position
-            ->map(fn ($p, $z) => $crabs->reduce(fn ($a, $c) => abs($c - $p) + $a, 0))
-            ->sort()
-            ->first()
-        ;
+        return $this->fuelHistogram($input, $deltaReduce);
     }
 
     public function solvePart2(): ?int
     {
         $input = array_map('intval', explode(',', $this->input[0]));
+        // this histogram is based on the sum of all numbers between crab and target position
+        $sumDeltaReduce = function (int $from, int $to): int {
+            $delta = abs($from - $to);
+
+            return (int) ($delta * ($delta + 1) / 2);
+        };
+
+        return $this->fuelHistogram($input, $sumDeltaReduce);
+    }
+
+    /**
+     * @param array<int, int> $input
+     * @param callable        $deltaReduce
+     *
+     * @return int
+     */
+    protected function fuelHistogram(array $input, callable $deltaReduce): int
+    {
         sort($input);
         $crabs     = collect($input);
         $positions = collect()->range($input[0], $input[count($input) - 1]);
 
         return (int) $positions
             // this histogram is based on the sum of all numbers between crab and target position
-            ->map(fn ($from) => $crabs->reduce(
-                // cache the range sum to save time
-                fn ($a, $to) => ($this->cache[abs($from - $to)] ??= array_sum(range(1, abs($from - $to)))) + $a,
+            ->map(fn (int $from) => $crabs->reduce(
+            // cache the range sum to save time
+                fn (int $carry, int $to) => $deltaReduce($from, $to) + $carry,
                 0
             ))
             ->sort()
