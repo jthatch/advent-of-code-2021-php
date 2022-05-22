bce6369eca650b085bf3b14d29a2596a50f037f7
James Thatcher <james@jameswt.com>
2022-05-23 00:30:42 +0100

    Day9 part 1 solved

---
+++ src/Day9.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Contracts\DayBehaviour;
+
+/**
+ * Very similar to Day11 on AOC 2020.
+ *
+ * @see https://github.com/jthatch/advent-of-code-php-2020/blob/master/src/Day11.php
+ */
+class Day9 extends DayBehaviour
+{
+    // [[y,x],..] each of the 4 possible adjacent locations, starting top going clockwise
+    private array $adjacent = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
+    /**
+     * Working with y,x coordinates isn't really collect()'s strong suite, so did this the old-fashioned way.
+     *
+     * @return int|null
+     */
+    public function solvePart1(): ?int
+    {
+        $heightmap = array_map(static fn (string $s): array => array_map('intval', str_split(trim($s))), $this->input);
+
+        $yMax = count($heightmap);
+        $xMax = count($heightmap[0]);
+        $low  = [];
+
+        for ($y = 0; $y < $yMax; ++$y) {
+            for ($x = 0; $x < $xMax; ++$x) {
+                $location = $heightmap[$y][$x];
+
+                // loop over our adjacent positions, if none are bigger then we've found a low point
+                if (empty(array_filter(
+                    $this->adjacent,
+                    fn (array $pos) => null !== ($heightmap[$y + $pos[0]][$x + $pos[1]] ?? null) && $location >= $heightmap[$y + $pos[0]][$x + $pos[1]]
+                ))) {
+                    $low[] = $location;
+                }
+            }
+        }
+
+        return collect($low)->reduce(fn (int $c, int $n) => (1 + $n) + $c, 0);
+    }
+
+    public function solvePart2(): ?int
+    {
+        // TODO: Implement solvePart2() method.
+        return null;
+    }
+
+    public function example(): array
+    {
+        return explode("\n", <<<eof
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+eof);
+    }
+}
