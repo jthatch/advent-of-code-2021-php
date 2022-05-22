18560c77882e68e935050ba26de2ccf3d94d8675
James Thatcher <james@jameswt.com>
2022-05-22 14:28:52 +0100

    Day2 refactor using collect()

--- src/Day2.php
+++ src/Day2.php
@@ -8,9 +8,30 @@ use App\Contracts\DayBehaviour;
 
 class Day2 extends DayBehaviour
 {
+    /**
+     *  Refactored this using collect, which seems a few orders of magnitude slower :(
+     *  Mem[616kb] Peak[ 1013kb] Time[0.06339s]   <-- Using collect()
+     *  Mem[422kb] Peak[  925kb] Time[0.00072s]   <-- Using commented out array functionality.
+     *
+     * @return int|null
+     */
     public function solvePart1(): ?int
     {
-        $input = array_map(fn (string $line) => explode(' ', $line), $this->input);
+        $pos = collect($this->input)
+            ->reduce(function (array $pos, string $cmd) {
+                [$instruction, $value] = explode(' ', $cmd);
+                match ($instruction) {
+                    'forward' => $pos['hoz'] += (int) $value,
+                    'down'    => $pos['dep'] += (int) $value,
+                    'up'      => $pos['dep'] -= (int) $value,
+                };
+
+                return $pos;
+            }, ['hoz' => 0, 'dep' => 0]);
+
+        return $pos['hoz'] * $pos['dep'];
+
+        /*$input = array_map(fn (string $line) => explode(' ', $line), $this->input);
         $pos = [
             'hoz' => 0,
             'dep' => 0,
@@ -24,32 +45,29 @@ class Day2 extends DayBehaviour
             };
         }
 
-        return $pos['hoz'] * $pos['dep'];
+        return $pos['hoz'] * $pos['dep'];*/
     }
 
     public function solvePart2(): ?int
     {
-        $input = array_map(fn (string $line) => explode(' ', $line), $this->input);
-        $pos = [
-            'hoz' => 0,
-            'dep' => 0,
-            'aim' => 0,
-        ];
+        $pos = collect($this->input)
+            ->reduce(function (array $pos, string $cmd) {
+                [$instruction, $value] = explode(' ', $cmd);
+                switch ($instruction) {
+                    case 'forward':
+                        $pos['hoz'] += (int) $value;
+                        $pos['dep'] += (int) $value * $pos['aim'];
+                        break;
+                    case 'down':
+                        $pos['aim'] += (int) $value;
+                        break;
+                    case 'up':
+                        $pos['aim'] -= (int) $value;
+                        break;
+                }
 
-        foreach ($input as [$instruction, $value]) {
-            switch ($instruction) {
-                case 'forward':
-                    $pos['hoz'] += (int) $value;
-                    $pos['dep'] += (int) $value * $pos['aim'];
-                    break;
-                case 'down':
-                    $pos['aim'] += (int) $value;
-                    break;
-                case 'up':
-                    $pos['aim'] -= (int) $value;
-                    break;
-            }
-        }
+                return $pos;
+            }, ['hoz' => 0, 'dep' => 0, 'aim' => 0]);
 
         return $pos['hoz'] * $pos['dep'];
     }
