6a4ed1d378f27bed016d3a7c35cb74f823f14d02
James Thatcher <james@jameswt.com>
2022-05-30 00:25:14 +0100

    Day 10 part 2 completed

--- src/Day10.php
+++ src/Day10.php
@@ -5,58 +5,65 @@ declare(strict_types=1);
 namespace App;
 
 use App\Contracts\DayBehaviour;
+use Illuminate\Support\Collection;
 
 class Day10 extends DayBehaviour
 {
+    protected array $tags = [
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
+    ];
+
     public function solvePart1(): ?int
     {
-        $tags = [
-            '(' => ')',
-            '[' => ']',
-            '{' => '}',
-            '<' => '>',
-        ];
-
         return collect($this->input)
-            ->map(
-                function (string $line) use ($tags) {
-                    $tree = [];
-
-                    $errors = collect(str_split($line))
-                        ->filter(function (string $t) use ($tags, &$tree) {
-                            $isOpenTag = isset($tags[$t]);
-                            if ($isOpenTag) {
-                                $tree[] = $t;
-                            } else {
-                                $lastOpen   = last($tree);
-                                $isValidTag = $t === $tags[$lastOpen];
-                                if ($isValidTag) {
-                                    array_pop($tree);
-                                } else {
-                                    return true;
-                                }
-                            }
-
-                            return false;
-                        });
-
-                    return $errors->isNotEmpty() ? $errors->first() : null;
-                }
-            )
-            ->filter()
-            ->reduce(fn ($c, $t) => $c + match ($t) {
-                ')' => 3,
-                ']' => 57,
-                '}' => 1197,
-                '>' => 25137,
+            ->map(fn (string $line): ?string => $this->parseChunk($line)['errors']->first())
+            ->reduce(fn (int $c, ?string $t): int => $c + match ($t) {
+                ')'     => 3,
+                ']'     => 57,
+                '}'     => 1197,
+                '>'     => 25137,
+                default => 0,
             }, 0)
         ;
     }
 
     public function solvePart2(): ?int
     {
-        // TODO: Implement solvePart2() method.
-        return null;
+        return (int) collect($this->input)
+            ->map(fn (string $line): array  => $this->parseChunk($line))
+            ->map(fn (array $chunk): ?array => $chunk['errors']->isEmpty() ? collect($chunk['chunk'])->map(fn ($t) => $this->tags[$t])->reverse() : null)
+            ->filter()
+            ->map(fn ($chunk) => $chunk->reduce(fn ($c, $t) => (5 * $c) + match ($t) {')' => 1, ']' => 2, '}' => 3, '>' => 4,}, 0))
+            ->median();
+    }
+
+    /**
+     * @param string $line
+     * @return array{chunk: array, errors: Collection}
+     */
+    protected function parseChunk(string $line): array
+    {
+        $chunk  = [];
+        $errors = collect(str_split($line))
+            ->filter(function (string $t) use (&$chunk) {
+                if (isset($this->tags[$t])) { // we've found an open tag, add to our chunk
+                    $chunk[] = $t;
+                } elseif ($t === $this->tags[last($chunk)] ?? null) { // found a valid closing tag
+                    array_pop($chunk);
+                } else { // syntax error
+                    return true;
+                }
+
+                return false;
+            });
+
+        return [
+            'chunk'  => $chunk,
+            'errors' => $errors,
+        ];
     }
 
     public function example(): array
